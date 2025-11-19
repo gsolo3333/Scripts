44e7d6c4cfa0d35b05edd1f8981b24c8841af3bd
gsolo3333 <sben30051@gmail.com>
2025-11-19 21:13:48 +0100

    Update BloxFruitsv2.lua

--- Games/BloxFruitsv2.lua
+++ Games/BloxFruitsv2.lua
@@ -1,1 +1,30 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/gsolo3333/Library/refs/heads/main/solo-V5-remake/main.luau"))()
+
+local Window = Library:MakeWindow({
+  Title = "Solo Hub : Blox Fruits ",
+  SubTitle = "by solo3333",
+  ScriptFolder = "solo-library-V5"
+})
+
+local Minimizer = Window:NewMinimizer({
+  KeyCode = Enum.KeyCode.LeftControl
+})
+
+local MobileButton = Minimizer:CreateMobileMinimizer({
+  Image = "rbxassetid://15298567397",
+  BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+})
+
+local Tab = Window:MakeTab({
+  Title = "Main",
+  Icon = "Home"
+})
+
+Window:Notify({
+  Title = "Notification",
+  Content = "Solo hub loded",
+  Image = "rbxassetid://0",
+  Duration = 5
+})
+
 
