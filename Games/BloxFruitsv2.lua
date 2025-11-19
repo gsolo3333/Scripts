55681485e840b040702cfd58bd860241715c6d48
gsolo3333 <sben30051@gmail.com>
2025-11-19 21:22:44 +0100

    Update BloxFruitsv2.lua

--- Games/BloxFruitsv2.lua
+++ Games/BloxFruitsv2.lua
@@ -27,4 +27,10 @@ Window:Notify({
   Duration = 5
 })
 
-
+Window:Notify({ 
+  if Sea1 then
+     Title = "Notification",
+     Content = "this is a Notification",
+     Image = "rbxassetid://10734953451",
+     Duration = 5
+})
